69bc36731b4ef31ea14773becf5eeaf48d9d2674
Paula Minni <paula@Paulas-MacBook-Pro.local>
2017-09-18 23:19:46 +0300

    brainfuck parser

--- brainfuck_parser.js
+++ brainfuck_parser.js
@@ -1,7 +1,3 @@
-var Paula = "++++++++[>+++[>+++>++++>+++++>+++++<<<<-]>+>>>--[<]<-]>>.>+.>---.>++++.<<.";
-var HelloWorld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
-
-
 function brainfuck_parser(code) {
 
   var cells = [];
@@ -18,29 +14,22 @@ function brainfuck_parser(code) {
     //console.log("i: " , i)
 
     if (code.charAt(i) === '+') {
-      //console.log("cells[cellIndex]: ", cells[cellIndex])
-
       if(cells[cellIndex] != undefined) {
         var item = cells[cellIndex];
-        //console.log("item: " , item)
         cells.splice(cellIndex,1);
         item++;
         cells.splice(cellIndex,0,item);
-        //console.log("cells: " , cells)
       }
       else cells.push(1);
       //console.log("+++: ", cells[cellIndex])
     }
 
     else if (code.charAt(i) === '-') {
-      //console.log("joo-")
       if(cells[cellIndex] != undefined) {
         var item = cells[cellIndex];
-        //console.log("item: " , item)
         cells.splice(cellIndex,1);
         item--;
         cells.splice(cellIndex,0,item);
-        //console.log("cells: " , cells)
       }
       else cells.push(1);
       //console.log("---: " , cells[cellIndex])
@@ -57,16 +46,9 @@ function brainfuck_parser(code) {
     }
 
     else if (code.charAt(i) === '[') {
-      //i = loopEnd.pop();
-      //console.log("joo: " , cells[cellIndex])
       if (cells[cellIndex] === 0) {
-        //console.log("menee tänne")
         i = loopEnd.pop();
-        //console.log("loopend: " , loopEnd , i)
       }
-
-      //else loopEnd.pop();
-
       else {
         loopStart.push(i);
         loopEnd.pop();
@@ -88,6 +70,9 @@ function brainfuck_parser(code) {
   }
   return toPrint;
 }
+
+var Paula = "++++++++[>+++[>+++>++++>+++++>+++++<<<<-]>+>>>--[<]<-]>>.>+.>---.>++++.<<.";
+var HelloWorld = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
 
 console.log(brainfuck_parser(Paula))
 console.log(brainfuck_parser(HelloWorld))
